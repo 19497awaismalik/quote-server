849a8136a3882833b459f6df1b64c0e7af14651e
Awais Malik <165843085+19497awaismalik@users.noreply.github.com>
2024-08-26 23:27:58 +0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 
 
 const Connection=async()=>{
-   await mongoose.connect("mongodb://localhost:27017")
+   await mongoose.connect("mongodb+srv://19497awaismalik:awaismalik@cluster0.naygr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
    console.log(`Connect With MongoDB Successfully`)
 }
